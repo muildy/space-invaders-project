66ec90189f879e0ad25bc84125151b29af320482
muildy <101232272+muildy@users.noreply.github.com>
2022-05-26 09:24:21 +1000

    messing around in title screen
    added cool colours and shapes to title screen

--- Space Invaders/RaylibStarterCPP/TitleScreen.cpp
+++ Space Invaders/RaylibStarterCPP/TitleScreen.cpp
@@ -9,45 +9,65 @@ struct Star {
     int y;
 
     Color colour;
+
+    
 };
 
 
 TitleScreen::TitleScreen(int screenWidth, int screenHeight)
 {
-    const int starsAmount = 100;
+    const int starsAmount = 800;
     Star* stars[starsAmount];
 
-    for (int i = 0; i < starsAmount; i++) {
-        stars[i] = new Star;
-        stars[i]->x = rand() % screenWidth;
-        stars[i]->y = rand() % screenHeight;
-        stars[i]->colour.a = rand() % 255;
-        stars[i]->colour.r = rand() % 255;
-        stars[i]->colour.g = rand() % 255;
-        stars[i]->colour.b = rand() % 255;
-    }
-
+    //for (int i = 0; i < starsAmount; i++) {
+    //    stars[i] = new Star;
+    //    stars[i]->x = rand() % screenWidth;
+    //    stars[i]->y = rand() % screenHeight;
+    //    stars[i]->colour.a = (rand() + 100) % 255;
+    //    stars[i]->colour.r = (rand() + 100) % 255;
+    //    stars[i]->colour.g = (rand() + 100) % 200;
+    //    stars[i]->colour.b = (rand() + 100) % 255;
+    //}
+    int counter = 0;
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-
+        counter++;
 
         // Update
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
         for (int i = 0; i < starsAmount; i++) {
-            DrawCircle(stars[i]->x, stars[i]->y, 2, stars[i]->colour);
+            stars[i] = new Star;
+            stars[i]->x = i;//rand() % screenWidth;
+            stars[i]->y = sin(i) * 100;//rand() % screenHeight;
+            stars[i]->colour.a = (rand() + 100) % 255;
+            stars[i]->colour.r = (rand() + 100) % 255;
+            stars[i]->colour.g = (rand() + 100) % 200;
+            stars[i]->colour.b = (rand() + 100) % 255;
         }
-
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(BLACK);
 
+
+
+        for (int i = 0; i < starsAmount; i++) {
+            DrawCircle(stars[i]->x, stars[i]->y, 3, stars[i]->colour);
+            stars[i] = nullptr;
+            delete(stars[i]);
+        }
+
+
         EndDrawing();
         //---------------------------------------------------------------------------------- 
     }
+    //for (int i = 0; i < starsAmount; i++) {
+    //    stars[i] = nullptr;
+    //    delete(stars[i]);
+    //}
 }
 
 TitleScreen::~TitleScreen()
